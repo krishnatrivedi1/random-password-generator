168f893148e5eb63cd79ab528af243e5a7e15e11
krishna trivedi <krishutrivedi2000@gmail.com>
2026-05-24 22:53:05 +0530

    delete comment

--- main.js
+++ main.js
@@ -1,45 +1,4 @@
-// const btn = document.querySelector(".btn")
-// const input = document.querySelector(".input")
-// const copy = document.querySelector(".fa-copy")
-// const alertcontainer = document.querySelector(".alert-container")
 
-// btn.addEventListener("click" , () => {
-// createPassword()
-// })
-
-// copy.addEventListener("click" , () =>{
-//     copypasword()
-//     if(input.value){
-// alertcontainer.classList.remove("active")
-//      setTimeout(() => {
-//         alertcontainer.classList.add("active")
-//     },2000)
-//     }
-   
-    
-// })
-// function createPassword(){
-//     const chars = "0123456789abcdefghijklmnopqrstuvwxtzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//     const passwordLength = 8
-//     let password = ""
-//     for (let index = 0; index <passwordLength; index++) {
-//       const randomNum = Math.floor(Math.random() * chars.length);
-//       console.log(randomNum);
-      
-//       password += chars.substring(randomNum , randomNum + 1)
-//         // console.log(randomNum , password);
-//       console.log(chars.substring(randomNum , randomNum +1));
-      
-        
-//         input.value = password
-//     }
-//     alertcontainer.innerHTML = password + " copied!"
-// }
-// function copypasword(){
-//     input.select() 
-//     input.setSelectionRange(0, 9999);
-//     navigator.clipboard.writeText(input.value)
-// }
 
 
 const btn = document.querySelector(".btn");
